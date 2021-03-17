e8a81e019cf8b5bca80f2f1d46f6bf407f409bed
scu <1373685219@qq.com>
2021-03-16 18:38:13 -0700

    recode the program

---
+++ CGImysql/sql_connection_pool.h
@@ -0,0 +1,63 @@
+#ifndef _CONNECTION_POOL_
+#define _CONNECTION_POOL_
+
+#include <stdio.h>
+#include <list>
+#include <mysql/mysql.h>
+#include <error.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include "../lock/locker.h"
+#include "../log/log.h"
+
+using namespace std;
+
+class connection_pool
+{
+public:
+	MYSQL *GetConnection();				 //获取数据库连接
+	bool ReleaseConnection(MYSQL *conn); //释放连接
+	int GetFreeConn();					 //获取连接
+	void DestroyPool();					 //销毁所有连接
+
+	//单例模式 这个是利用局部静态变量懒汉模式实现单例（多线程不友好）
+	//对于多线程而言，多个线程可能同时访问到该静态变量，并发现其没有被初始化（C++实现机制是）
+	//（该看静态变量内部一标志位是为1，为1则说明已被初始化）
+	//多个线程同时判定其没有初始化而后均初始化就会造成错误（即它不是一个原子操作）
+	static connection_pool *GetInstance();
+
+	void init(string url, string User, string PassWord, string DataBaseName, int Port, int MaxConn, int close_log); 
+
+private:
+	connection_pool();
+	~connection_pool();
+
+	int m_MaxConn;  //最大连接数
+	int m_CurConn;  //当前已使用的连接数
+	int m_FreeConn; //当前空闲的连接数
+	locker lock;
+	list<MYSQL *> connList; //连接池
+	sem reserve;
+
+public:
+	string m_url;			 //主机地址
+	string m_Port;		 //数据库端口号
+	string m_User;		 //登陆数据库用户名
+	string m_PassWord;	 //登陆数据库密码
+	string m_DatabaseName; //使用数据库名
+	int m_close_log;	//日志开关
+};
+
+class connectionRAII{
+
+public:
+	connectionRAII(MYSQL **con, connection_pool *connPool);
+	~connectionRAII();
+	
+private:
+	MYSQL *conRAII;
+	connection_pool *poolRAII;
+};
+
+#endif
---
+++ threadpool/threadpool.h
@@ -0,0 +1,160 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <list>
+#include <cstdio>
+#include <exception>
+#include <pthread.h>
+#include "../lock/locker.h"
+#include "../CGImysql/sql_connection_pool.h"
+
+template <typename T>
+class threadpool
+{
+public:
+    /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
+    threadpool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_request = 10000);
+    ~threadpool();
+    bool append(T *request, int state);
+    bool append_p(T *request);
+
+private:
+    /*工作线程运行的函数，它不断从工作队列中取出任务并执行之*/
+    static void *worker(void *arg);//为什么要用静态成员函数呢？
+    void run();
+
+private:
+    int m_thread_number;        //线程池中的线程数
+    int m_max_requests;         //请求队列中允许的最大请求数
+    pthread_t *m_threads;       //描述线程池的数组，其大小为m_thread_number
+    std::list<T *> m_workqueue; //请求队列
+    locker m_queuelocker;       //保护请求队列的互斥锁
+    sem m_queuestat;            //是否有任务需要处理
+    connection_pool *m_connPool;  //数据库
+    int m_actor_model;          //模型切换（这个切换是指？）
+};
+template <typename T>
+threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int thread_number, int max_requests) : m_actor_model(actor_model),m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL),m_connPool(connPool)
+{
+    if (thread_number <= 0 || max_requests <= 0)
+        throw std::exception();
+    m_threads = new pthread_t[m_thread_number];     //pthread_t是长整型
+    if (!m_threads)
+        throw std::exception();
+    for (int i = 0; i < thread_number; ++i)
+    {
+        //创建成功应该返回0，如果线程池在线程创建阶段就失败，那就应该关闭线程池了
+        //函数原型中的第三个参数，为函数指针，指向处理线程函数的地址。
+        //该函数，要求为静态函数。如果处理线程函数为类成员函数时，需要将其设置为静态成员函数。
+        //因为指向的线程处理函数参数类型为(void *),若线程函数为类成员函数，
+        //则this指针会作为默认的参数被传进函数中，从而和线程函数参数(void*)不能匹配，不能通过编译。
+        //静态成员函数就没有这个问题，因为里面没有this指针。
+        if (pthread_create(m_threads + i, NULL, worker, this) != 0)
+        {
+            delete[] m_threads;
+            throw std::exception();
+        }
+        //主要是将线程属性更改为unjoinable，便于资源的释放，详见PPPPS
+        if (pthread_detach(m_threads[i]))
+        {
+            delete[] m_threads;
+            throw std::exception();
+        }
+    }
+}
+template <typename T>
+threadpool<T>::~threadpool()
+{
+    delete[] m_threads;
+}
+template <typename T>
+bool threadpool<T>::append(T *request, int state)
+{
+    m_queuelocker.lock();
+    if (m_workqueue.size() >= m_max_requests)
+    {
+        m_queuelocker.unlock();
+        return false;
+    }
+    //说实话，这个模板T哪来的成员变量m_state？（未知）
+    request->m_state = state;
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+template <typename T>
+bool threadpool<T>::append_p(T *request)//有关模板的性质，先看http的解决方式（因为在这运用了它）
+{
+    m_queuelocker.lock();
+    if (m_workqueue.size() >= m_max_requests)
+    {
+        m_queuelocker.unlock();
+        return false;
+    }
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+template <typename T>
+void *threadpool<T>::worker(void *arg)
+{
+    threadpool *pool = (threadpool *)arg;
+    pool->run();
+    return pool;
+}
+template <typename T>
+void threadpool<T>::run()
+{
+    while (true)
+    {
+        m_queuestat.wait();
+        m_queuelocker.lock();
+        if (m_workqueue.empty())
+        {
+            m_queuelocker.unlock();
+            continue;
+        }
+        T *request = m_workqueue.front();
+        m_workqueue.pop_front();
+        m_queuelocker.unlock();
+        if (!request)
+            continue;
+        if (1 == m_actor_model)
+        {
+            if (0 == request->m_state)
+            {
+                if (request->read_once())
+                {
+                    request->improv = 1;
+                    connectionRAII mysqlcon(&request->mysql, m_connPool);
+                    request->process();
+                }
+                else
+                {
+                    request->improv = 1;
+                    request->timer_flag = 1;
+                }
+            }
+            else
+            {
+                if (request->write())
+                {
+                    request->improv = 1;
+                }
+                else
+                {
+                    request->improv = 1;
+                    request->timer_flag = 1;
+                }
+            }
+        }
+        else
+        {
+            connectionRAII mysqlcon(&request->mysql, m_connPool);
+            request->process();
+        }
+    }
+}
+#endif
